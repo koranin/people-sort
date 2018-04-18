9641f79ec365c832cefce4c675d6aac7861c8d6f
koranins <ksu@earnest.com>
2018-04-17 21:33:18 -0700

    Refactor PersonComparator class.

--- src/PersonComparator.java
+++ src/PersonComparator.java
@@ -13,32 +13,63 @@ public class PersonComparator implements Comparator<Person> {
   
   @Override
   public int compare(final Person firstPerson, final Person secondPerson) {
-    return 0;
+    switch (sortField) {
+      case "ssn": 
+        return compare(firstPerson.ssn, secondPerson.ssn);
+      case "dateOfBirth":
+        return compare(firstPerson.dateOfBirth, secondPerson.dateOfBirth);
+      case "firstName":
+        return compare(firstPerson.firstName, secondPerson.firstName);
+      case "lastName":
+        return compare(firstPerson.lastName, secondPerson.lastName);
+      case "heightIn":
+        return compare(firstPerson.heightIn, secondPerson.heightIn);
+      case "weightLb":
+        return compare(firstPerson.weightLb, secondPerson.weightLb);
+    }
   }
 
-  private static Comparator<String> getStringComparator(final Boolean isAscending) {
-    return (a, b) -> isAscending ? compareString(a, b) : compareString(b, a);
+  /**
+   * Compares its two string arugments for order.
+   */
+  private int compare(final String firstvalue, final String secondValue) {
+    return ascending ? compare(firstvalue, secondValue) : compare(secondValue, firstValue)
   }
 
-  private static int compareString(final String a, final String b) {
+  /**
+   * Compare two strings.
+   */
+  private static int compare(final String firstValue, final String secondValue) {
     // TODO: implement here
     return 0;
   }
- 
-  public static Comparator<Date> getDateComparator(final Boolean isAscending) {
-    return (a, b) -> isAscending ? compareDate(a, b) : compareDate(b, a);
+
+  /**
+   * Compare its two date arguments for order.
+   */
+  public static int compare(final Date firstDate, final Date secondDate) {
+    return ascending ? compare(firstDate, secondDate) : compare(secondDate, firstDate);
   }
-  
-  private static int compareDate(final Date a, final Date b) {
+
+  /**
+   * Compare two dates.
+   */
+  private static int compare(final Date a, final Date b) {
     // TODO: implement here
     return 0;
   }
- 
-  private static Comparator<Double> getDoubleComparator(final Boolean isAscending) {
-    return (a, b) -> isAscending ? compareDouble(a, b) : compareDouble(b, a);
+
+  /**
+   * Compare its two double arguments for order.
+   */
+  private static int compare(final Double firstDouble, final Double secondDouble) {
+    return isAscending ? compare(a, b) : compare(b, a);
   }
 
-  private static int compareDouble(final Double a, final Double b) {
+  /**
+   * Compare two doubles.
+   */
+  private static int compare(final Double a, final Double b) {
     // TODO: implement here
     return 0;
   }
