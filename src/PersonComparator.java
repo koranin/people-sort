139bee2d7881751f44726bcfce4316fee0cbac65
koranins <ksu@earnest.com>
2018-04-17 22:34:02 -0700

    Add sorting/comparing logic to PersonComparator

--- src/PersonComparator.java
+++ src/PersonComparator.java
@@ -1,6 +1,14 @@
-import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
+import java.util.function.BiFunction;
 import java.util.List;
+import java.lang.Double;
+import java.time.LocalDate;
 
 public class PersonComparator implements Comparator<Person> {
   private final String compareField;
@@ -13,7 +21,7 @@ public class PersonComparator implements Comparator<Person> {
   
   @Override
   public int compare(final Person firstPerson, final Person secondPerson) {
-    switch (sortField) {
+    switch (compareField) {
       case "ssn": 
         return compare(firstPerson.ssn, secondPerson.ssn);
       case "dateOfBirth":
@@ -26,51 +34,60 @@ public class PersonComparator implements Comparator<Person> {
         return compare(firstPerson.heightIn, secondPerson.heightIn);
       case "weightLb":
         return compare(firstPerson.weightLb, secondPerson.weightLb);
+      default:
+        return 0;
     }
   }
 
   /**
    * Compares its two string arugments for order.
    */
-  private int compare(final String firstvalue, final String secondValue) {
-    return ascending ? compare(firstvalue, secondValue) : compare(secondValue, firstValue)
-  }
-
-  /**
-   * Compare two strings.
-   */
-  private static int compare(final String firstValue, final String secondValue) {
-    // TODO: implement here
-    return 0;
+  private int compare(final String firstString, final String secondString) {
+    return compareWithOrder(firstString, secondString, PersonComparator::compareStrings);
   }
 
   /**
    * Compare its two date arguments for order.
    */
-  public static int compare(final Date firstDate, final Date secondDate) {
-    return ascending ? compare(firstDate, secondDate) : compare(secondDate, firstDate);
-  }
-
-  /**
-   * Compare two dates.
-   */
-  private static int compare(final Date a, final Date b) {
-    // TODO: implement here
-    return 0;
+  public int compare(final Date firstDate, final Date secondDate) {
+    return compareWithOrder(firstDate, secondDate, PersonComparator::compareDates);
   }
 
   /**
    * Compare its two double arguments for order.
    */
-  private static int compare(final Double firstDouble, final Double secondDouble) {
-    return isAscending ? compare(a, b) : compare(b, a);
+  private int compare(final Double firstDouble, final Double secondDouble) {
+    return compareWithOrder(firstDouble, secondDouble, PersonComparator::compareDoubles);
+  }
+
+  /**
+   * Compares its two arguments with either ascending or descending order.
+   */
+  private <T> int compareWithOrder(final T firstValue, final T secondValue, final Comparator<T> comparator) {
+    return ascending ? comparator.compare(firstValue, secondValue) : comparator.compare(secondValue, firstValue);
+  }
+
+  /**
+   * Compare two strings.
+   */
+  private static int compareStrings(final String firstString, final String secondString) {
+    return firstString.compareTo(secondString);
+  }
+
+  /**
+   * Compare two dates.
+   */
+  private static int compareDates(final Date firstDate, final Date secondDate) {
+    final SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyyMMdd");
+    final String firstDateString = dateFormatter.format(firstDate);
+    final String secondDateString = dateFormatter.format(secondDate);
+    return compareStrings(firstDateString, secondDateString);
   }
 
   /**
    * Compare two doubles.
    */
-  private static int compare(final Double a, final Double b) {
-    // TODO: implement here
-    return 0;
+  private static int compareDoubles(final Double firstDouble, final Double secondDouble) {
+    return Double.compare(firstDouble, secondDouble);
   }
 }
